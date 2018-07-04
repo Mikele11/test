0e7a2ea9202f8d3cf395c09a35972c2c1c7bc35a
Mikele11 <mihailo_rlyash@mail.ru>
2018-07-04 16:39:48 +0300

    Update server.js
    change server

--- server.js
+++ server.js
@@ -22,16 +22,6 @@ MongoClient.connect(MONGO_URL, function(err, db){
 			//db.close();
 		});
 	});
-	/*
-	app.get('/', function (req, res) {
-		return res.redirect('/chat');//'/app'
-	});
-	*/
-	  /*
-	  db.collection("bookday").find(function (err, docs) {
-		console.log(docs);
-		res.json(docs);
-		*/
 
 	app.post('/chat', function (req, res) {
 	  console.log(req.body);
@@ -55,48 +45,14 @@ MongoClient.connect(MONGO_URL, function(err, db){
 
 
 var bodyParser = require('body-parser');
-//---------------------------------------------swagger
 const swaggerUi = require('swagger-ui-express');
-
 const swaggerDocument = require('./swagger.json');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-//---------------------------------------------
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-/*
-app.get('/chat', function (req, res) {
-  console.log('All right');
-  db.collection("bookday").find(function (err, docs) {
-    console.log(docs);
-    res.json(docs);
-  })
-});
 
-app.post('/chat', function (req, res) {
-  console.log(req.body);
-  db.collection("bookday").insert(req.body, function(err, doc) {
-    res.json(doc);
-  });
-});
-
-app.delete('/chat/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(id);
-  db.collection("bookday").remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
-    res.json(doc);
-  });
-});
-*/
-/*
-app.get('/chat/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(id);
-  db.chat.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
-    res.json(doc);
-  });
-});
-*/
 const port = process.env.PORT || 3000;
 app.listen(port);
 console.log("Server running on port 3000");
