be118464ca36217ff2e51c6aab9cc2d2a8a1056f
Mikele11 <mihailo_rlyash@mail.ru>
2018-07-09 05:15:21 +0300

    Update controller.js

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -29,7 +29,8 @@ $scope.addContact = function() {
 		};
 
 		$scope.contact.registr=d2;
-		$scope.contact.name=$('input').val();
+		$scope.contact.name=$('.name').val();
+		$scope.contact.avatar=$('#picrscr').val();
 		$http.post('/chat', $scope.contact).success(function(response) {
 			refresh();
 		});
